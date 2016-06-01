943bf56f9d0718d173ce55f26b341a0849e2fc07
blueish <samchowa@gmail.com>
2016-06-01 15:40:14 -0700

    Got the sql to work, it creates and drops the tables

--- createtables.sql
+++ createtables.sql
@@ -1,21 +1,21 @@
-DROP TABLE Page;
-DROP TABLE Poster;
-DROP TABLE Thread;
-DROP TABLE UserComment;
-DROP TABLE ThreadVote;
-DROP TABLE CommentVote;
-DROP TABLE Admin;
 DROP TABLE Suggestion;
+DROP TABLE CommentVote;
+DROP TABLE ThreadVote;
+DROP TABLE UserComment;
+DROP TABLE Thread;
+DROP TABLE Poster;
+DROP TABLE Page;
 
 CREATE TABLE Page (
-	TopicName CHAR(100),
+	TopicName VARCHAR2(100),
 	AdminPageFlag INTEGER,
 	PRIMARY KEY (TopicName));
 
 CREATE TABLE Poster (
-	PosterName CHAR(50),
+	PosterName VARCHAR2(50),
 	Reputation INTEGER,
 	numberOfPages INTEGER,
+  AdminID INTEGER,
 	PRIMARY KEY (PosterName));
 
 CREATE TABLE Thread (
@@ -25,9 +25,9 @@ CREATE TABLE Thread (
 	Time CHAR(12),
 	voteNum INTEGER,
 	isLockedFlag INTEGER,
-	TopicName CHAR(100),
-	PosterName CHAR(50),
-	PRIMARY KEY (Title, ThreadID, TopicName),
+	TopicName VARCHAR2(100),
+	PosterName VARCHAR2(50),
+	PRIMARY KEY (ThreadID),
 	FOREIGN KEY (TopicName) REFERENCES Page,
 	FOREIGN KEY (PosterName) REFERENCES Poster);
 
@@ -35,15 +35,15 @@ CREATE TABLE UserComment (
 	CommID INTEGER,
 	Text VARCHAR2(4000),
 	voteNum INTEGER,
-	Time CHAR(12),
-	PosterName CHAR(50),
+	Time TIMESTAMP,
+	PosterName VARCHAR2(50),
 	ThreadID INTEGER,
 	PRIMARY KEY(CommID),
-	FOREIGN KEY (PosterName) REFERENCES Poster,
-	FOREIGN KEY (ThreadID) REFERENCES Thread);
+	FOREIGN KEY (ThreadID) REFERENCES Thread,
+	FOREIGN KEY (PosterName) REFERENCES Poster);
 
 CREATE TABLE ThreadVote (
-	PosterName CHAR(50),
+	PosterName VARCHAR2(50),
 	UserVotedFlag INTEGER,
 	ThreadID	INTEGER,
 	PRIMARY KEY (PosterName, ThreadID),
@@ -51,7 +51,7 @@ CREATE TABLE ThreadVote (
 	FOREIGN KEY (ThreadID) REFERENCES Thread);
 
 CREATE TABLE CommentVote (
-	PosterName CHAR(50),
+	PosterName VARCHAR2(100),
 	UserVotedFlag INTEGER,
 	ThreadID	INTEGER,
   CommID INTEGER,
@@ -60,20 +60,15 @@ CREATE TABLE CommentVote (
 	FOREIGN KEY (CommID) REFERENCES UserComment);
 	
 
-CREATE TABLE Admin (
-	AdminName CHAR(50) PRIMARY KEY);
-
--- TODO :  admin controls tables
-
-
 CREATE TABLE Suggestion (
 	SugID INTEGER,
 	Text VARCHAR2(4000),
-	PosterName CHAR(50),
-	TopicName CHAR(100),
+	PosterName VARCHAR2(100),
+	TopicName VARCHAR2(100),
 	PRIMARY KEY (SugID, PosterName, TopicName),
 	FOREIGN KEY (PosterName) REFERENCES Poster,
 	FOREIGN KEY (TopicName) REFERENCES Page);
+
 
 
 
