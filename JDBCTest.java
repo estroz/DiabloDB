146a6c760efb596e3147855c534ccfccbdefba73
blueish <samchowa@gmail.com>
2016-06-03 16:34:15 -0700

    Added the DB Test Connection in Java

---
+++ JDBCTest.java
@@ -0,0 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCTest {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+		System.out.println("Yay it worked");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:ug",
+				"INSERTNAME", "INSERTPASSWORD");
+		System.out.println("conencted to db");
+		Statement stmt = con.createStatement();
+		
+		ResultSet rs = stmt.executeQuery("SELECT * FROM Poster");
+		
+		
+		System.out.println(rs.toString());
+		
+		while (rs.next()) {
+			String name = rs.getString("PosterName");
+			int rep = rs.getInt(2);
+			int numPages = rs.getInt(3);
+			int adminID = rs.getInt(4);
+			System.out.println(name);
+			System.out.print(rep);
+			System.out.print(numPages);
+			System.out.print(adminID);
+			System.out.println("");
+		}
+
+		con.close();
+		System.out.println("dc'd from db");
+
+	}
+
+}
