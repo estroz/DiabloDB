02a037f7b7edfc2145b8acaed18124d38c9f18d3
Eric Stroczynski <ericstroczynski@gmail.com>
2016-06-10 15:24:50 -0700

    added methods to check if poster is an admin and to lock a thread

--- src/Poster.java
+++ src/Poster.java
@@ -1,3 +1,9 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
 
 public class Poster {
 
@@ -5,6 +11,8 @@ public class Poster {
 	public int reputation;
 	public int numberOfPages;
 	public Integer adminID;
+
+	private Connection connection;
 
 	public Poster(String userName, int reputation, int numberOfPages, Integer adminID) {
 		this.userName = userName;
@@ -16,8 +24,35 @@ public class Poster {
 			this.adminID = adminID;
 		}
 	}
-	
+
 	public String toString() {
 		return "Poster Object, name: " + userName + " rep: " + Integer.toString(reputation);
 	}
+
+
+	/*
+	 * Returns true if a poster has an adminID
+	 */
+	public boolean isPosterAdmin(String posterName) throws SQLException {
+		Statement stmt = this.connection.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT AdminID FROM Poster WHERE PosterName = '"+posterName+"'");
+
+		String adminID = rs.getString("AdminID");
+		if (!adminID.wasNull()) return true;
+		return false;
+	}
+
+	/*
+	 * Lock a thread
+	 * TODO: figure out how to lock thread instance so no more comments can be added
+	 */
+	public void lockThread(int threadID, String posterName) throws SQLException {
+		if (!isPosterAdmin(posterName)) {
+			throw new SQLException("You are not an administrator and thus cannot lock a thread.");
+		}
+		Statement stmnt = this.connection.createStatement();
+		int lockedThread = stmnt.executeUpdate("UPDATE UserThread SET isLockedFlag = 1 WHERE ThreadID = '"+threadID+"'");
+		assert lockedThread == 0;
+	}
+
 }
