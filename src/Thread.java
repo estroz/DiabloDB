645d47381eb6897869cf722f76d7641885a6bf9d
Eric Stroczynski <ericstroczynski@gmail.com>
2016-06-10 15:25:34 -0700

    added method to check if thread is locked

--- src/Thread.java
+++ src/Thread.java
@@ -10,7 +10,7 @@ public class Thread {
 	boolean isLocked;
 	String topicName;
 	String posterName;
-	
+
 	public Thread(int threadID, String title, String text, Time time, int voteNum, boolean isLocked, String topicName, String posterName) {
 		this.threadID = threadID;
 		this.title = title;
@@ -21,9 +21,20 @@ public class Thread {
 		this.topicName = topicName;
 		this.posterName = posterName;
 	}
-	
+
 	public String toString() {
 		return "ThreadObject: " + this.title + " & votecount: " + Integer.toString(this.voteNum);
 	}
-	
+
+	/*
+	 * Returns true if a thread is locked
+	 */
+	public boolean isThreadLocked(String threadID) throws SQLException {
+		Statement stmt = this.connection.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT isLockedFlag FROM Thread WHERE ThreadID = '"+threadID+"'");
+
+	  int flag = rs.getInt(6);
+		if (flag == 1) return true;
+		return false;
+	}
 }
